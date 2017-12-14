b4109b3c3d22d1ee08ddf8afd6c0d567fccfe813
John Rey Alipe <John Rey Alipe@SernoiLaptop>
2017-12-14 20:51:03 +0800

    With MainFrame

---
+++ src/com/shs/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shs.main;
+
+/**
+ *
+ * @author John Rey Alipe
+ */
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        MainFrame mf = new MainFrame();
+        mf.setVisible(true);
+    }
+}
